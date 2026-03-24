aee9db0f8f388ab46af3d85c79a01dac5096f985
Guillermo <panahermoso10@gmail.com>
2026-03-24 15:02:09 -0600

    profile views added with viewsets

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,5 +5,22 @@ from .serializers import UserSerializer, ProducerProfileSerializer, CompanyProfi
 # Create your views here.
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows users to be viewed.
+    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+class ProducerProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for viewing and editing producer profiles.
+    """
+    queryset = ProducerProfile.objects.all()
+    serializer_class = ProducerProfileSerializer
+
+class CompanyProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for viewing and editing company profiles.
+    """
+    queryset = CompanyProfile.objects.all()
+    serializer_class = CompanyProfileSerializer
